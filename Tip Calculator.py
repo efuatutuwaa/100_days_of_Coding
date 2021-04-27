07feba1c217521579907fad27eca70dc943883c3
Afua <etutuwaa.88@gmail.com>
2021-04-27 23:41:35 +0000

    tip calculator

---
+++ Tip Calculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Tip Calculator 💸")
+bill = float(input("What is the total bill?: "))
+tip = int(input("How much tip would you like to give: 10,12 or 15? or enter a custom tip: "))
+people = int(input("How many people are splitting the bill?: "))
+
