8c42e629ca1f250e7ed3cd589968f85c495699df
Afua <etutuwaa.88@gmail.com>
2021-04-28 00:02:00 +0000

    tip calculator

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,5 +1,13 @@
 print("Welcome to the Tip Calculator 💸")
-bill = float(input("What is the total bill?: "))
+bill = float(input("What is the total bill?: GHS "))
 tip = int(input("How much tip would you like to give: 10,12 or 15? or enter a custom tip: "))
 people = int(input("How many people are splitting the bill?: "))
+
+tip_as_percent = tip/100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = "{:.2f}".format(bill_per_person)
+
+print(f"Each person would pay : GHC {final_amount}")
 
