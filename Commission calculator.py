becce1cc38e4442f12aec6d050eb501e85ef3976
Afua <etutuwaa.88@gmail.com>
2021-05-01 14:58:58 +0000

    Update Commission calculator.py

--- Commission calculator.py
+++ Commission calculator.py
@@ -9,10 +9,9 @@ final_commission_amount ="{:.2f}".format(total_commission_amount)
 net_earnings = gross_earnings - total_commission_amount
 final_net_earnings = "{:.2f}".format(net_earnings)
 
-<<<<<<< HEAD
+
 print(f"Your commission on GHC {final_gross_earning} is GHC {final_commission_amount}")
 print(f"You get to keep GHC {final_net_earnings} as your net earning, after paying your commission.")
-=======
 print(f"Your commission on  GHC{final_gross_earning} is GHC {final_commission_amount}")
 print(f"You get to keep  GHC {final_net_earnings} as your net earnings, after paying your commission.")
->>>>>>> origin/master
+
